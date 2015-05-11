f768df44cf91ba6977072194e60c570b28bc4a6d
Jonas Wissing <j_wiss04@uni-muenster.de>
2015-05-11 12:22:59 +0200

    added routes
    added all routes so not all have to modify the same file

--- Project/js/route.js
+++ Project/js/route.js
@@ -1,6 +1,4 @@
-// script.js
-
-    // create the module and name it scotchApp
+    // create the module and name it App
         // also include ngRoute for all our routing needs
     var App = angular.module('App', ['ngRoute']);
 
@@ -8,15 +6,35 @@
     App.config(function($routeProvider) {
         $routeProvider
 
-            // route for the home page
+            // route for the login page
             .when('/', {
                 templateUrl : 'templates/login.html',
                 controller  : 'loginController'
             })
+            
+            .when('/gameselection', {
+                templateUrl : 'templates/selectgame.html',
+                controller : 'selectGameController'
+            })
+            
+            .when('/gameinfo', {
+                templateUrl : 'templates/gameinfo.html',
+                controller : 'gameInfoController'
+            })
+            
+            .when('/game', {
+                templateUrl : 'templates/game.html',
+                controller : 'gameController'
+            })
+
+            .when('/profile', {
+                templateUrl : 'templates/profile.html',
+                controller : 'profileController'
+            })
 
             // route for the contact page
             .when('/signup', {
-                templateUrl : 'templates/signUp.html',
-                controller  : 'signupController'
+                templateUrl : 'templates/signup.html',
+                controller  : 'signUpController'
             });
     });
