cbffeddc4fbb81f9448110571209d0f42d155526
crend02 <c_rend02@wwu.de>
2015-06-08 11:25:24 +0200

    www hinzugefügt

---
+++ Project/www/js/gameselect.js
@@ -0,0 +1,13 @@
+App.controller("selectGameController", function ($scope, $http, $routeParams) {
+	
+
+	this.tab = 1;
+	this.selectTab = function (setTab) {
+		this.tab = setTab;
+	};
+	this.isSelected = function (checkTab) {
+		return this.tab === checkTab;
+	};
+	
+	$scope.token = $routeParams.token;
+});
